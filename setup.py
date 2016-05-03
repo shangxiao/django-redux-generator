b86dd8130878e40cb76d9cf2b2650781042c025a
David Sanders <dsanders@rapilabs.com>
2016-05-03 22:06:57 +1000

    Setup package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-redux-generator',
+    version='0.1.0',
+    description='Generate redux boilerplate',
+    author='David Sanders',
+    author_email='dsanders@rapilabs.com',
+    packages=find_packages(),
+    package_data={
+        'django_redux_generator': ['templates/django_redux_generator/*.js'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+    ],
+    install_requires=[
+        'Django',
+        'inflection',
+    ],
+)
